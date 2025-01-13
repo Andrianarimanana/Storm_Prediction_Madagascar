7b93d8dd1d930f5e584f45d466f6b50ca4c38f1c
ANDRIANARIMANANA Nomenjanahary Philippe <narimanana2020@gmail.com>
2025-01-14 01:50:48 +0300

    final Xboost model

--- train.py
+++ train.py
@@ -13,8 +13,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import LogisticRegression
+
+import xgboost as xgb
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import roc_auc_score
+from sklearn.tree import export_text
 
 # parameters
 
@@ -110,13 +113,54 @@ test_df = add_lag_features(test_df, lag_features, lag_intervals)
 train_df = add_size_features(train_df)
 test_df = add_size_features(test_df)
 
-train_df.head(3)
+#train_df.head(3)
 
 # training 
+# Prepare Training data
+feature_cols = [
+    'hour_sin', 'hour_cos', 
+    'distance_x', 'distance_y', 'intensity', 'size', 'distance',
+    'is_peak_cyclone_season', 
+    'cyclone_season_weight', 'peak_cyclone_daytime_interaction', 'size_change_30', 'bearing'
+]
 
+# Add lag columns to feature_cols
+for feat in lag_features:
+    for lag_min in lag_intervals.keys():
+        feature_cols.append(f"{feat}_{lag_min}")
+
+X = train_df[feature_cols]
+y_1h = train_df['Storm_NosyBe_1h']
+y_3h = train_df['Storm_NosyBe_3h']
 print(f'doing validation with C={C}')
+#spilit 
+
+x_full_train, x_test,y1h_full_train, y1h_test= train_test_split(X, y_1h, test_size=0.2, random_state=11)
+x_train, x_val, y1h_train, y1h_val= train_test_split(x_full_train,y1h_full_train, test_size=0.25, random_state=11)
+
+_, _, y3h_full_train, y3h_test = train_test_split(X, y_3h, test_size=0.2, random_state=11)
+_, _, y3h_train, y3h_val= train_test_split(x_full_train,y3h_full_train, test_size=0.25, random_state=11)
+
+
+df_train = x_train.reset_index(drop=True)
+df_val = x_val.reset_index(drop=True)
+df_test = x_test.reset_index(drop=True)
+
+df_full_train = x_full_train.reset_index(drop=True)
 
 # training the final model
+dicts_full_train = df_full_train.fillna(0).to_dict(orient='records')
+
+dv = DictVectorizer(sparse=False)
+X_full_train = dv.fit_transform(dicts_full_train)
+
+dicts_test = df_test.fillna(0).to_dict(orient='records')
+X_test = dv.transform(dicts_test)
+
+dfulltrain = xgb.DMatrix(X_full_train, label=y1h_full_train,
+                    feature_names=dv.get_feature_names_out())
+
+dtest = xgb.DMatrix(x_test, label=y1h_test, feature_names=dv.get_feature_names_out)
 print('training the final model')
 
 # Save the model
